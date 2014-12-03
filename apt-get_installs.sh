3663d0007fefe3ed20d50b2a529751bee0fa36be
Blake Visin <Blake@oybro.com>
2014-12-03 14:19:14 -0800

    Added an apt-get install packages script to keep track of what packages I want to install on a new pi

---
+++ apt-get_installs.sh
@@ -0,0 +1,8 @@
+#a list of things I want to install to get these scripts to be able to run
+
+sudo apt-get install feh
+
+sudo apt-get install qiv
+
+#screen saver that may be able to do what I want to do but better...need to try it on the pi
+sudo apt-get install xscreensaver xscreensaver-gl-extra xscreensaver-data-extra
